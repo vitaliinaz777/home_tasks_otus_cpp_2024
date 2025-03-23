6e6f242dd557628ffc9d0c925531276462d4760a
Vitalii Nazarov <vitaliinaz777@gmail.com>
2025-03-24 00:26:55 +0300

    fix lesson_3 guess_the_number.cpp

--- 03.homework/guess_the_number.cpp
+++ 03.homework/guess_the_number.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include "print_arguments.h"
 #include "make_random_value.h"
@@ -13,26 +14,51 @@ int main(int argc, char** argv) {
         return result;
     }
 
-    // Game loop
     const int target_value = make_random_value();
-	int current_value = 0;
+	int current_value{};
 	bool not_win = true;
+	int attempts{};
+	std::string name{};
+	const std::string filename = "Score.txt";
+	
+
+	// Game loop
+	std::cout << "Hi! Enter your name, please:" << std::endl;
+	std::cin >> name;	
 	std::cout << "Enter your guess:" << std::endl;
 	do {
+		attempts++;
+		
 		std::cin >> current_value;
 
 		if (current_value < target_value) {
-			std::cout << "less than " << current_value << std::endl;
-		}
-		else if (current_value > target_value) {
 			std::cout << "greater than " << current_value << std::endl;
 		}
+		else if (current_value > target_value) {
+			std::cout << "less than " << current_value << std::endl;
+		}
 		else {
-			std::cout << "you win!" << std::endl;
+			std::cout << "you win!" << " attempts: " << attempts << std::endl;
 			break;
 		}
 
 	} while(true);
 
-	return 0;
+	// Save the score
+	// Открываем файл для записи и чтения
+	std::fstream io_file{filename, std::ios_base::in | std::ios_base::out | std::fstream::app};
+	// Записываем в конец файла
+	io_file << name << " " << attempts << std::endl;
+	io_file.flush();
+	// Перемещаем каретку в начало файла
+	io_file.seekg(0);
+	
+	std::cout << "High scores table: " << std::endl;
+	// Читаем файл построчно
+	while(io_file >> name >> attempts) {
+		std::cout << name << " " << attempts << std::endl;
+	}
+
+	// Закрываем файл
+	io_file.close();
 }
